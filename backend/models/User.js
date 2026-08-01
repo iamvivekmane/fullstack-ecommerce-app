3fe241ea7f046a3b6362c0d5bd62a7e5d6d66080
Vivek Mane <vivekmane172@gmail.com>
2026-08-01 10:45:24 +0530

    Made changes in User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['user', 'admin'], default: 'user' },
-    phone: { type: varchar, required: true }
+    phone: { type: String, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
