9d55bbeaeb15c7b834fd7111592f7bffecacb4a1
Vivek Mane <vivekmane172@gmail.com>
2026-08-02 09:48:58 +0530

    Added addresses field in user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const usersSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['user', 'admin'], default: 'user' },
-    phone: { type: String, required: true }
+    phone: { type: String, required: true },
+    address: { street: String, city: String, postalCode: String, country: String },
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Users', usersSchema);
