c50fb75db6bc1fa934764d0d6daf46a2ddc25b68
Vivek Mane <vivekmane172@gmail.com>
2026-08-02 10:49:21 +0530

    Fixed error

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -8,12 +8,12 @@ const orderSchema = new mongoose.Schema({
         price: { type: Number, required: true },
         quantity: { Type: Number, required: true, default: 1 }
     }],
-    shippingAdress: { street: { type: String }, city: { type: String }, state: { type: String }, zip: { type: String }, country: { type: String }, },
+    shippingAddress: { street: { type: String }, city: { type: String }, state: { type: String }, zip: { type: String }, country: { type: String }, },
     paymentMethod: { type: String, default: 'razorpay' },
     paymentStatus: { type: String, enum: ['pending', 'paid', 'failed'], default: 'pending' },
     orderStatus: { type: String, enum: ['placed', 'processing', 'shipped', 'delivered', 'cancelled'], default: 'placed' },
     totalAmount: { type: Number, required: true }
-}, { timestamp: true })
+}, { timestamps: true })
 
 
 module.exports = mongoose.model("Order", orderSchema);
