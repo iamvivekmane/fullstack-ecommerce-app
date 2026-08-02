5013bdcb0e358a87cb235ac7f310e7e122839d74
Vivek Mane <vivekmane172@gmail.com>
2026-08-02 10:14:32 +0530

    Fixed category model by adding timestamps and fixing naming convention

---
+++ backend/models/Category.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const categorySchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    slug: { type: String, required: true, unique: true },
+    parent_category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true, default: null },
+    image: { type: String, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Category', categorySchema);
