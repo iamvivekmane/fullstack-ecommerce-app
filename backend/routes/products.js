e821e85b8037b53f595f6df91deb08883849708f
Vivek Mane <vivekmane172@gmail.com>
2026-08-06 17:37:00 +0530

    implemented put request /api/product/:id to update the product with the id

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -73,7 +73,7 @@ router.get('/getproducts', [
         return res.status(400).json({ errors: result.array() });
     }
     try {
-        let products = await Product.find({ brand: brand });
+        let products = await Product.find();
         success = true;
         res.json({ products })
         //Catches the error
@@ -104,6 +104,58 @@ router.get('/getproducts/:id', [
         res.status(500).send("Internal server error");
     }
 })
+
+
+// Updates the product with the id
+router.put('/updateproduct/:id', [
+    body('name', 'name is not valid').isLength({ min: 3 }),
+    body('slug', 'slug is not valid').isLength(3),
+    body('description', 'description is not valid').isLength(3),
+    body('price', 'price is not valid').isLength(1),
+    body('discountPrice', 'price is not valid').isLength(1),
+    body('brand', 'brand is not valid').isLength(1),
+    body('stock', 'stock is not valid').isLength(1),
+    body('images', 'images is not valid').isLength(3),
+    body('isFeatured', 'images is not valid').isBoolean(),
+    body('ratings', 'ratings is not valid').isLength(3),
+], async (req, res) => {
+    let success = false;
+
+    let id = req.params.id;
+
+    console.log(id);
+    console.log(req.body);
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        console.log("getting")
+        return res.status(400).json({ errors: result.array() });
+    }
+    try {
+        let product = await Product.updateOne({ _id: id }, {
+            $set: {
+                name: req.body.name,
+                slug: req.body.slug,
+                description: req.body.description,
+                price: req.body.price,
+                discountPrice: req.body.discountPrice,
+                category: req.body.category,
+                brand: req.body.brand,
+                stock: req.body.stock,
+                images: req.body.images,
+                isFeatured: req.body.isFeatured,
+                ratings: req.body.ratings
+            }
+        });
+        success = true;
+        res.json({ product })
+        //Catches the error
+    } catch (error) {
+        res.status(500).send("Internal server error");
+    }
+})
+
+
+
 
 
 
