bb8a5cf44178da251db0662cd25aae4d0b9c30d7
Vivek Mane <vivekmane172@gmail.com>
2026-08-19 18:18:30 +0530

    implemented isadmin middleware for protected routes

---
+++ backend/middleware/isadmin.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = process.env.JWT_SECRET
+
+const isadmin = (req, res, next) => {
+    //Get user from JWT token and add id to the request object
+    const token = req.header('auth-token')
+    if (!token) {
+        return res.status(401).send({ error: "Please authenticate using a valid token" })
+    }
+    try {
+        const data = jwt.verify(token, JWT_SECRET)
+        req.user = data.user
+        req.role = data.user.role;
+        if (req.role != 'admin') {
+            return res.status(401).send({ error: "Please authenticate admin token" })
+        }
+        next();
+    } catch (error) {
+        return res.status(401).send({ error: "Please authenticate using a valid token" })
+    }
+
+}
+module.exports = isadmin;
